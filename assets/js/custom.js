ed9e0ebc0aea1629f4f89cf8177577aff0c80df4
abbasinfoicon <uid3@infoiconsoftware.com>
2021-08-07 13:32:06 +0530

    home

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,7 +7,7 @@ $('.arrival-carousel').owlCarousel({
     margin:10,
     responsiveClass:true,
     dots: false,
-    navText: ["<img src='assets/img/left-red-icon.png' class='img-responsive' />", "<img src='assets/img/right-red-icon.png' class='img-responsive' />"],
+    navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
     responsive:{
         0:{
             items:1,
@@ -31,21 +31,21 @@ $('.arrival-carousel').owlCarousel({
 
 $('.deal-section').owlCarousel({
     loop:true,
-    margin:10,
+    margin:30,
     responsiveClass:true,
-    navText: ["<img src='assets/img/left-red-icon.png' class='img-responsive' />", "<img src='assets/img/right-red-icon.png' class='img-responsive' />"],
+    nav: false,
+    dots: true,
     responsive:{
         0:{
             items:1,
-            nav:true
+            margin: 0
         },
         600:{
             items:1,
-            nav:false
+            margin: 0
         },
         1000:{
             items:2,
-            nav:true,
             loop:false
         }
     }
@@ -68,6 +68,36 @@ $('#scroll').click(function () {
 
 
 /*========================================================================================================
-================================ pensions_carousel ===================================================================
+================================ Auto TIMEING ===================================================================
 ===========================================================================================================*/
 
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hr").innerHTML = hours;
+  document.getElementById("min").innerHTML = minutes;
+  document.getElementById("sec").innerHTML = seconds;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("day").innerHTML = "EXPIRED";
+  }
+}, 1000);
